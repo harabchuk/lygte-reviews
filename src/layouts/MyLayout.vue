14bba2a9de11887a019498dfc1d2ba4fd041f63a
Aliaksei Harabchuk <aliaksei.harabchuk@kognity.com>
2019-12-18 13:35:39 +0100

    Chargers initial screen

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -7,15 +7,14 @@
           dense
           round
           @click="leftDrawerOpen = !leftDrawerOpen"
-          icon="menu"
+          icon="fas fa-bars"
           aria-label="Menu"
         />
 
         <q-toolbar-title>
-          Quasar App
+          Reviews from lygte-info.dk
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -26,59 +25,34 @@
       content-class="bg-grey-2"
     >
       <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+        <q-item-label header>Sections</q-item-label>
+
+        <q-item clickable to="/">
           <q-item-section avatar>
-            <q-icon name="school" />
+            <q-icon name="fas fa-charging-station" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
+            <q-item-label>Chargers</q-item-label>
+            <q-item-label caption>Battery chargers reviews</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
+
+        <q-item clickable to="/d">
           <q-item-section avatar>
-            <q-icon name="code" />
+            <q-icon name="fas fa-battery-three-quarters" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
+            <q-item-label>Batteries</q-item-label>
+            <q-item-label caption>Rechargarble batteries reviews</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+
+        <q-item clickable tag="a" target="_blank" href="https://lygte-info.dk/">
           <q-item-section avatar>
-            <q-icon name="chat" />
+            <q-icon name="far fa-copyright" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="record_voice_over" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="public" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Facebook</q-item-label>
-            <q-item-label caption>@QuasarFramework</q-item-label>
+            <q-item-label>lygte-info.dk</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
