c2fe8b73b30d37e1cfb318c68eeefa5e16f03792
Aliaksei Harabchuk <aliaksei.harabchuk@kognity.com>
2020-01-15 10:29:14 +0100

    Added batteries reviews

--- src/pages/BatteryTypes.vue
+++ src/pages/BatteryTypes.vue
@@ -6,8 +6,8 @@
                     title="AA and AAA batteries" 
                     subTitle="Alkaline, lithium, NiMH"
                     imageSrc="/statics/batteries/images/AA-AAA-batteries.jpg"
-                    routeToList="{name: 'chargers'}"
-                    routeToComparison="{name: 'chargers'}"
+                    :routeToList="{name: 'chargers'}"
+                    :routeToComparison="{name: 'chargers'}"
                 />
             </div>
             <div class="col-md-4 col-sm-8 col-xs-12">
@@ -15,8 +15,8 @@
                     title="9V batteries" 
                     subTitle="Alkaline, lithium, NiMH etc."
                     imageSrc="/statics/batteries/images/9v-batteries.jpg"
-                    routeToList="{name: 'chargers'}"
-                    routeToComparison="{name: 'chargers'}"
+                    :routeToList="{name: 'chargers'}"
+                    :routeToComparison="{name: 'chargers'}"
                 />
             </div>
             <div class="col-md-4 col-sm-8 col-xs-12">
@@ -24,8 +24,8 @@
                     title="C and D batteries"
                     subTitle="Alkaline, lithium, NiMH"
                     imageSrc="/statics/batteries/images/C-D-batteries.jpg"
-                    routeToList="{name: 'chargers'}"
-                    routeToComparison="{name: 'chargers'}"
+                    :routeToList="{name: 'chargers'}"
+                    :routeToComparison="{name: 'chargers'}"
                 />
             </div>
         </div>   
